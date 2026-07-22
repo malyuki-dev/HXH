cfcfec27c341825c74ae9b8524aa477955851a67
malyuki-dev <malyuki@dev.local>
2026-07-22 15:07:36 -0300

    Add Alpha Content Pack 16: Emperor Time

--- data/creaturescripts/scripts/logout.lua
+++ data/creaturescripts/scripts/logout.lua
@@ -1,7 +1,14 @@
 function onLogout(player)
-	local playerId = player:getId()
-	if nextUseStaminaTime[playerId] then
-		nextUseStaminaTime[playerId] = nil
+	-- Remove Emperor Time if active
+	local STORAGE_EMPEROR_TIME = 85000
+	if player:getStorageValue(STORAGE_EMPEROR_TIME) > 0 then
+		player:setStorageValue(STORAGE_EMPEROR_TIME, 0)
 	end
+
+	local nextUseStaminaTime = player:getStorageValue(Storage.Stamina)
+	if nextUseStaminaTime ~= -1 then
+		player:setStorageValue(Storage.Stamina, nextUseStaminaTime + 10)
+	end
+
 	return true
 end
